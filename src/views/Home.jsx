9cc41123fc087979455e3ba0ba4a73515ec68a53
Alexandros Botzis <alexbotzis@gmail.com>
2021-05-01 23:26:09 +0200

    Remove redundant comments

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,7 +1,4 @@
 import React from "react";
-// import { useSelector } from "react-redux";
-// import Error from "./Error";
-// import { CircularProgress } from "@material-ui/core";
 import Main from "../components/Main";
 
 const Home = () => <Main />;
