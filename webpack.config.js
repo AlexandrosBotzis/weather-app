e0c185aa027400afa78a68e1798f876ea911b40d
Alexandros Botzis <alexbotzis@gmail.com>
2021-05-02 12:59:43 +0200

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/weather-app/',
+  },
+  devServer: {
+    compress: true,
+    historyApiFallback: true,
+    hot: true,
+    open: true,
+    overlay: true,
+    port: 8000,
+    stats: {
+      normal: true,
+    },
+  },
+  module: {
+    rules: [
+      {
+        test:  /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html'),
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  resolve: {
+    modules: ['node_modules', path.resolve(__dirname, 'app')],
+    extensions: ['*', '.js', '.jsx', '.css'],
+  },
+};
